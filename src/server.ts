243d01467f3313673c7751820b3bb7a2b21423b7
Daniel Guerra <danielgguerra2@gmail.com>
2021-07-16 08:37:10 -0300

    implementacao de documentacao com swagger UI

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,6 @@
 import 'express-async-errors';
+import swaggerUI from 'swagger-ui-express';
+import swaggerFile from '../swagger_output.json';
 import express, { Request, Response } from 'express';
 import "reflect-metadata";
 import './infrastruture/database';
@@ -10,5 +12,7 @@ const app = express();
 app.use(express.json());
 app.use(router);
 app.use(errorMiddleware);
+
+app.use('/doc', swaggerUI.serve, swaggerUI.setup(swaggerFile));
 
 app.listen(3333, () => console.log('Server is runner on port 3333'));
